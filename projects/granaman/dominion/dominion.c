924f55c187632823baa30be618d1f553629ae3f4
Mandi Grant <mandi018@gmail.com>
2019-10-07 13:10:25 -0500

    Adding a bug to the baron method

--- projects/granaman/dominion/dominion.c
+++ projects/granaman/dominion/dominion.c
@@ -724,7 +724,7 @@ int cardBaron(int currentPlayer, int choice1, struct gameState *state)
             gainCard(estate, state, 0, currentPlayer);//Gain an estate
 
             state->supplyCount[estate]--;//Decrement Estates
-            if (supplyCount(estate, state) == 0) {
+            if (supplyCount(estate, state) = 0) {
                 isGameOver(state);
             }
         }
