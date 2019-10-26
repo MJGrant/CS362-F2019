4059e7ae873554a45c844f6085878933486681d4
Mandi Grant <mandi018@gmail.com>
2019-10-25 21:58:15 -0500

    Added assert methods specifically for increased by one and decreased by one since this kind of check is so common and just seems to read better than remembering to add +1 or -1 to the value I'm testing

--- projects/granaman/dominion/my_asserts.c
+++ projects/granaman/dominion/my_asserts.c
@@ -20,3 +20,21 @@ void assertNotEqual(char *testName, int param1, int param2) {
         printf("x -- %s [FAILED!] %d and %d expected to be inequal.\n", testName, param1, param2);
     }
 }
+
+// The tests frequently verify that a value was increased by 1
+void assertIncreasedByOne(char *testName, int before, int after) {
+    if (after - before == 1) {
+        printf("✔ -- %s [Passed] %d is one greater than %d.\n", testName, after, before);
+    } else {
+        printf("x -- %s [FAILED!] %d was expected to be one greater than %d.\n", testName, after, before);
+    }
+}
+
+// The tests frequently verify that a value was decreased by 1
+void assertDecreasedByOne(char *testName, int before, int after) {
+    if (before - after == 1) {
+        printf("✔ -- %s [Passed] %d is one less than %d.\n", testName, after, before);
+    } else {
+        printf("x -- %s [FAILED!] %d was expected to be one less than %d.\n", testName, after, before);
+    }
+}
--- projects/granaman/dominion/my_asserts.h
+++ projects/granaman/dominion/my_asserts.h
@@ -5,5 +5,7 @@
 
 void assertEqual(char *testName, int param1, int param2);
 void assertNotEqual(char *testName, int param1, int param2);
+void assertIncreasedByOne(char *testName, int before, int after);
+void assertDecreasedByOne(char *testName, int before, int after);
 
 #endif
