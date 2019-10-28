e482e8607828b1509f95d96a95fdf16de2e50599
Mandi Grant <mandi018@gmail.com>
2019-10-27 21:35:03 -0500

    Wired up unittest3.c for compiling and output

---
+++ projects/granaman/dominion/unittest3.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "my_asserts.h"
+#include "dominion.h"
+#include <time.h>
+
+// ************************
+// Unit tests for Ambassador card
+
+// initializeGame params: int numPlayers, int kingdomCards[10], int randomSeed, struct gameState *state
+
+// cardAmbassador params: int currentPlayer, int choice1, int choice2, struct gameState *state, int handPos)
+//choice1 represents the card the player has chosen to discard
+//choice2 represents how many of that card the player has chosen to discard
+
+int main() {
+
+    //ambassadorTest1a();
+
+    printf("[Ambassador Card test] Test complete\n");
+    return 0;
+}
